e1224a4998e816d905da8bb2e5fb9a0965f834f3
Yomaira Escano <yjescano@gmail.com>
2018-09-15 01:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// function preventRefreshOnSubmit(){
-//     document.querySelector('form').addEventListener('submit', function(event){
-//       event.preventDefault()
-//     })
-// }
-// 
-// preventRefreshOnSubmit()
-// 
-// function retrieveEmployeeInformation() {
-//   let input = document.querySelector('input')
-//   return input.value
-// }
-// 
-// function addNewElementAsLi() {
-//   let employeeName = retrieveEmployeeInformation()
-//   let employeeList = document.querySelector('.employee-list')
-// 
-//   return employeeList.insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
-// }
-// 
-// function addNewLiOnClick() {
-//   let input = document.querySelector('input[type="submit"]')
-// 
-//   input.addEventListener('click', function(e) {
-//     addNewElementAsLi()
-//     document.querySelector('input').value = ""
-//   })
-// }
-// 
-// function clearEmployeeListOnLinkClick() {
-//   let link = document.querySelector('a')
-//   let ul = document.querySelector('ul')
-// 
-//   link.addEventListener('click', function(e) {
-//     ul.innerHTML = ""
-//   })
-// }
+function preventRefreshOnSubmit(){
+    document.querySelector('form').addEventListener('submit', function(event){
+      event.preventDefault()
+    })
+}
+
+preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  let input = document.querySelector('input')
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation()
+  let employeeList = document.querySelector('.employee-list')
+
+  return employeeList.insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick() {
+  let input = document.querySelector('input[type="submit"]')
+
+  input.addEventListener('click', function(e) {
+    addNewElementAsLi()
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+
+  link.addEventListener('click', function(e) {
+    ul.innerHTML = ""
+  })
+}
